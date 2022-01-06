3aec6a96a616c146a817ce7299ad64b9199988eb
AnHuszti <husztiandrea82@gmail.com>
2022-01-06 11:50:17 +0100

    ready

--- src/lesson-01.ts
+++ src/lesson-01.ts
@@ -5,7 +5,7 @@ export default class calculator {
     // Hozd létre a következő változókat a megfelelő típusannotációval.
     
     /** @type {Array} points - számok tömbje, 1-től 10-ig egyesével. */
-
+    points: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     /** 
      * A tömb minden eleme legyen egy objektum, amely egy felhasználót reprezentál.
@@ -13,7 +13,11 @@ export default class calculator {
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} users - minden elem egy felhasználó.
      */
-
+        users: Array<{name: string, email: string}> = [
+            { name: "Anna", email: "anna@gmail.com"},
+            { name: "Bella", email: "bella@gmail.com"},
+            { name: "Cili", email: "cili@gmail.com"}
+        ]
     
     /** 
      * A tömb minden eleme legyen egy objektum, amely egy terméket reprezentál.
@@ -22,12 +26,16 @@ export default class calculator {
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} products - minden elem egy termék.
      */    
+        products: Array<{name: string, price: number, stock: number, active?: boolean}> = [
+            { name: "Anna", price: 111, stock: 11, active: true},
+            { name: "Bella", price: 222, stock: 22, active: false },
+            { name: "Cili", price: 333, stock: 33 }
+        ]
 
-    
     /** 
      * Inicializálj egy tuple típust, azaz adj neki értéket is.
      * @type {Tuple} role - number, string, boolean. 
      */
-
+        role: [number, string, boolean] = [0, "null", false]
 
 }
